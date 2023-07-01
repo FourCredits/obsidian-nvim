49e5847823fe6812fe640e8c145d1baf9ea10762
Erik <erik.grundy@gmail.com>
2023-07-01 17:27:49 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Obsidian support in Neovim
+
+Has (rudimentary) support for Obsidian's style of file system
+
+## TODO list
+
+- [ ] Make completion trigger on entering "[["
+- [ ] When inserting notes, account for the case when multiple notes have the same name (and so you need to include a prefix to differentiate them)
+- [ ] Make a `setup` function to customise keybindings
+
+## Dependencies
+
+- [Telescope](https://github.com/nvim-telescope/telescope.nvim)
---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,76 @@
+local obsidian_link_text = function()
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local row = cursor[1] - 1
+    local col = cursor[2] + 1
+    local line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
+    for s, text, e in line:gmatch("()(%b[])()") do
+        if s <= col and col <= e then
+            return text:sub(3, -3)
+        end
+    end
+    return nil
+end
+
+string.split_by_lines = function(text)
+    local result = {}
+    for line in text:gmatch("([^\n]+)\n?") do
+        table.insert(result, line)
+    end
+    return result
+end
+
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local sorters = require("telescope.sorters")
+local themes = require("telescope.themes")
+
+local go_to_obsidian_link = function()
+    local link_text = obsidian_link_text()
+    if link_text == nil then
+        print('no obsidian link found on current line')
+        return
+    end
+    local search_results =
+        vim.fn.system({ 'fdfind', link_text, '--type=file' }):split_by_lines()
+    if #search_results == 0 then
+        print('no file found matching that link')
+        return
+    elseif #search_results == 1 then
+        vim.cmd('e ' .. search_results[1])
+    else
+        pickers.new({}, {
+            prompt_title = "Select matching link",
+            finder = finders.new_table(search_results),
+            sorter = sorters.get_substr_matcher()
+        }):find()
+    end
+end
+
+vim.keymap.set('n', 'gd', go_to_obsidian_link, { noremap = true })
+
+local insert_obsidian_link = function()
+    local enter = function(prompt_bufnr)
+        local selected = action_state.get_selected_entry()[1]
+        -- TODO: doesn't account for multiple files with that name
+        -- That's a much harder problem, so I've left it for the moment
+        local extracted = selected:gsub("[^/]*/", ""):gsub("%.md", "")
+        local link = "[[".. extracted .. "]]"
+        actions.close(prompt_bufnr)
+        vim.api.nvim_put({ link }, "c", false, true)
+        vim.cmd("normal a")
+    end
+    pickers.new(themes.get_cursor(), {
+        prompt_title = "Select the file to insert a link to.",
+        finder = finders.new_oneshot_job({ "fdfind", "--type=file" }, {}),
+        sorter = sorters.get_fuzzy_file {},
+        attach_mappings = function(_, map)
+            map("i", "<cr>", enter)
+            map("n", "<cr>", enter)
+            return true
+        end,
+    }):find()
+end
+
+vim.keymap.set('i', '<c-x><c-f>', insert_obsidian_link, { noremap = true })
